edba1f483987e8a1a803dce16106f61fca3bf78a
Ruslan <ruslanfilistovich@gmail.com>
2019-08-10 20:52:42 +0300

    add two button (action 1 and action 2)

--- UIButtonAndUILabel/ViewController.swift
+++ UIButtonAndUILabel/ViewController.swift
@@ -11,40 +11,37 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var label: UILabel!
+    @IBOutlet var actionButtons: [UIButton]!
     @IBOutlet weak var button: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // скрываем label по умолчанию
         label.isHidden = true
-        // меняем размер текста
         label.font = label.font.withSize(35)
-        // меняем цвет текста
-        label.textColor = UIColor.white
         
-        //Добавляем текста в кнопку
-        button.setTitle("Get result", for: .normal)
-        // цвет текста кнопки
-        button.setTitleColor(.blue, for: .normal)
-        // цвет фона кнопки
-        button.backgroundColor = .yellow
+        button.isHidden = true
+        
+        for button in actionButtons {
+            button.setTitleColor(.blue, for: .normal)
+            button.backgroundColor = .yellow
+        }
+  
+        button.setTitle("Clear", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .red
     }
     
     @IBAction func pressedButton(_ sender: UIButton) {
         
-        if label.isHidden {
-            label.isHidden = false
-            label.text = "Hello, world!"
-            
-            button.setTitle("Clear", for: UIControl.State.normal)
-            button.setTitleColor(.white, for: .normal)
-            button.backgroundColor = .red
-        }else {
-            label.isHidden = true
-            
-            button.setTitle("Get result", for: .normal)
-            button.setTitleColor(.blue, for: .normal)
-            button.backgroundColor = .yellow
+        label.isHidden = false
+        button.isHidden = false
+        
+        if sender.titleLabel?.text == "Action 1" {
+            label.text = "Hello, World!"
+            label.textColor = .yellow
+        } else if sender.titleLabel?.text == "Action 2" {
+            label.text = "Hi, there!"
+            label.textColor = .blue
         }
     }
 }
